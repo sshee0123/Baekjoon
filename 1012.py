6b39ad02bb544ac5f974bdcf8ea443a474101697
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-03 00:23:28 +0900

    유기농배추_bfs

--- 1012.py
+++ 1012.py
@@ -1,9 +1,11 @@
 import sys
 sys.setrecursionlimit(10**9)
+from collections import deque
+T = int(input())
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 def dfs(x,y):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
     for i in range(4):
         nx = x+dx[i]
         ny = y+dy[i]
@@ -12,19 +14,33 @@ def dfs(x,y):
                 graph[nx][ny] = -1
                 dfs(nx,ny)
 
-T = int(input())
+def bfs(graph,x,y):
+    q = deque()
+    q.append((x,y))
+    graph[x][y] = 0
+    while q:
+        a,b = q.popleft()
+        for i in range(4):
+            nx = a+dx[i]
+            ny = b+dy[i]
+            # if nx<0 or nx>=M or ny<0 or ny>=N:
+            #     continue
+            if 0<=nx<M and 0<=ny<N and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                q.append((nx,ny))
+
 for _ in range(T):
     M,N,K = map(int,input().split())
-    graph = [[0]*M for _ in range(N)]
+    graph = [[0]*N for _ in range(M)]
     result = 0
 
     for _ in range(K):
         a,b = map(int,input().split())
-        graph[b][a] = 1
+        graph[a][b] = 1
 
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] > 0:
-                dfs(i,j)
+    for i in range(M):
+        for j in range(N):
+            if graph[i][j] ==1:
+                bfs(graph,i,j)
                 result+=1
     print(result)
