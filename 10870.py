ede1a603e69cf8c8e82d3dad3a8c0b3528a31c5b
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-03 23:39:31 +0900

    피보나치

--- 10870.py
+++ 10870.py
@@ -1,15 +1,15 @@
 n = int(input())
 
-#재귀함수
-def fib(n):
-    if n<=1:
-        return n
-    return fib(n-1)+fib(n-2)
-print(fib(n))
+# #재귀함수
+# def fib(n):
+#     if n<=1:
+#         return n
+#     return fib(n-1)+fib(n-2)
+# print(fib(n))
 
 #반복문
-fib_for = [0,1]
+fib = [0,1]
 for i in range(2,n+1):
-    n = fib_for[n-1]+fib_for[n-2]
-    fib_for.append(n)
-print(fib_for[n])
+    num = fib[i-1]+fib[i-2]
+    fib.append(num)
+print(fib[n])
