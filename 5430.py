0e014304a91bbb42b34dfeee5121d1eeee73d083
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-19 17:54:36 +0900

    AC

--- 5430.py
+++ 5430.py
@@ -1,36 +1,30 @@
 import sys
-from collections import deque
+
 t = int(sys.stdin.readline())
 
 for i in range(t):
-    p = list(sys.stdin.readline().rstrip())
+    p = sys.stdin.readline().rstrip()
     n = int(sys.stdin.readline())
     arr = sys.stdin.readline()[1:-2].rstrip().split(",")
-
-    if n ==0:
-        q = deque()
-    else:
-        q = deque(arr)
+    p = p.replace("RR","")
 
     rev = 0
+    f,b = 0,0
     flag = 0 #error 인지 확인
 
     for j in p:
         if j=="R":
             rev+=1
         elif j== "D":
-            if len(q)==0:
-                print("error")
-                flag = 1
-                break
+            if rev % 2 ==0:
+                f+=1
             else:
-                if rev % 2 ==0:
-                    q.popleft()
-                else:
-                    q.pop()
-    if flag == 0:
+                b+=1
+    if f+b <= n:
+        arr = arr[f:n-b]
         if rev % 2 ==0:
-            print("["+",".join(q)+"]")
+            print("["+",".join(arr)+"]")
         else:
-            q.reverse()
-            print("[" + ",".join(q) + "]")
+            print("[" + ",".join(arr[::-1]) + "]")
+    else:
+        print("error")
