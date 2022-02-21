8adf68ee7044a89058f845001e31853f032fe04f
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-21 17:43:13 +0900

    로또_dfs

--- 6603.py
+++ 6603.py
@@ -1,20 +1,43 @@
 import sys
 from itertools import combinations
 
+visited = [0] * 13
+def dfs(start,depth):
+    if depth == 6:
+        for i in range(k):
+            if visited[i]:
+                print(s[i],end=" ")
+        print()
+        return
+    for i in range(start,len(s)):
+        visited[i] = 1
+        dfs(i+1,depth+1)
+        visited[i] = 0
+    # if end == 6:
+    #     for i in range(6):
+    #         print(visited[i],end = " ")
+    #     print()
+    #     return
+    # for i in range(start,len(s)):
+    #     visited[end] = s[i]
+    #     dfs(i+1, end+1)
 while True:
     arr = list(map(int,sys.stdin.readline().split()))
     k = arr[0]
     if k == 0: #종료조건
         break
     s = arr[1:]
-
-    #조합
-    n = list(combinations(s,6))
-    # print(n)
-    for i in n:
-        for j in i:
-            print(j,end=" ")
-        print()
+    #dfs
+    dfs(0,0)
     print()
+    #
+    # #조합
+    # n = list(combinations(s,6))
+    # # print(n)
+    # for i in n:
+    #     for j in i:
+    #         print(j,end=" ")
+    #     print()
+    # print()
 
 
