c7947e86ba8f4395f64698627426cb33241b8a21
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-21 15:13:36 +0900

    2022/02/20

--- 2558.py
+++ 2558.py
@@ -2,8 +2,11 @@
 # b = int(input())
 # print(a+b)
 
-a = int(input())
-sum = 0
-for i in range(1,a+1):
-    sum+=i
-print(sum)
+# a = int(input())
+# sum = 0
+# for i in range(1,a+1):
+#     sum+=i
+# print(sum)
+
+import datetime
+print(str(datetime.datetime.now())[:10])
