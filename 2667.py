8914ada6d3e72e946347f273992650e24944f3c9
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-02-26 00:10:19 +0900

    bfs

--- 2667.py
+++ 2667.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+
 n = int(sys.stdin.readline())
 arr = []
 for i in range(n):
@@ -22,6 +24,29 @@ def dfs(x,y):
             return True
     else:
         return False
+
+def bfs(graph,x,y):
+    n = len(graph)
+    q = deque()
+    q.append((x,y))
+    graph[x][y] = 0
+    count = 1
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = 0
+                    q.append((nx,ny))
+                    count +=1
+    return count
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            cnt_arr.append(bfs(arr,i,j))
+
 for i in range(n):
     for j in range(n):
         if dfs(i,j) == True:
