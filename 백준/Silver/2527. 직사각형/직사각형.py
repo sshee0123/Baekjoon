3c275a38f31e8c8cc140366315599efc5b2f0233
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-03-11 16:37:27 +0900

    [Silver I] Title: 직사각형, Time: 84 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.py
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -1,29 +1,22 @@
 #2527
 import sys
 for i in range(4):
-    x1, y1, x2, y2, x3, y3, x4, y4 = map(int,sys.stdin.readline().split())
-    # 직사각형이 겹치기 위한 계산
-    # 왼쪽 변
-    xl = max(x1, x3)
-    # 오른쪽 변
-    xr = min(x2, x4)
-    # 윗 변
-    upr = min(y2,y4)
-    # 아랫 변
-    downl = max(y1, y3)
+    x1, y1, p1, q1, x2, y2, p2, q2 = map(int,sys.stdin.readline().split())
 
-    #겹치는 부분 (차이)
-    diffx = xr-xl
-    diffy = upr-downl
-
-    # 차이가 둘다 양수면 겹치는 부분이 직사각형
-    if diffx > 0 and diffy > 0:
-        print("a")
-    # 둘 다 0이면 점
-    elif diffx == 0 and diffy == 0:
-        print("c")
-    # 공통부분이 없음
-    elif diffx < 0 or diffy < 0:
+    if p1 < x2 or q1 < y2 or p2 < x1 or q2 < y1:
+        #겹치지 않음
         print("d")
-    else: # 선분
+        continue
+    elif p1 == x2 or p2 == x1:
+        if q1 == y2 or q2 == y1:
+            print("c") #점
+            continue
+        else:
+            print("b") #선분
+            continue
+    elif q1 == y2 or q2 == y1:
         print("b")
+        continue
+    else:
+        print("a") #직사각형
+        continue
