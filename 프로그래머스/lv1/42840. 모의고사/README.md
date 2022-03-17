bb85cb0f638350efaa1b2306c3729ce93e67ddf1
박세희 <55376366+sshee0123@users.noreply.github.com>
2022-03-17 22:19:16 +0900

    [level 1] Title: 모의고사, Time: 0.05 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.05 ms
+메모리: 4.18 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 //수포자 3명 규칙
@@ -21,8 +22,8 @@ vector<int> solution(vector<int> answers) {
         if (answers[i] == p3[i%10])
             score[2]++;
     }
-    max_score = max(max(score[0],score[1]), score[2]);
-
+    //max_score = max(max(score[0],score[1]), score[2]);
+    max_score = *max_element(score.begin(), score.end());
     for(int i=0; i<score.size();i++){
         if (score[i] == max_score){
             answer.push_back(i+1);
