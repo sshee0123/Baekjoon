5cc9261d3153a8a6642f2167d65dd8f469d09dfb
박세희 <55376366+sshee0123@users.noreply.github.com>
2022-03-18 22:10:21 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -4,10 +4,11 @@ using namespace std;
 bool visited[201]; //방문검사
 
 void dfs(vector<vector<int>> &arr, int idx){
+    visited[idx] = true;
     for(int i=0; i<arr[idx].size(); i++){
         if(arr[idx][i] == 1 && visited[i] == false){ //1이고 아직 방문하지 않은 컴
-            arr[idx][i] = 0;
-            visited[i] = true;
+            // arr[idx][i] = 0;
+            // visited[i] = true;
             dfs(arr, i); //dfs 진행
         }
     }
