978093dd7645832adc6b803804c80920d8395ca9
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-02-06 22:52:51 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,9 +1,10 @@
 def solution(a, b, c):
     answer = 0
-    if a != b and b!=c and a!=c:
-        answer = (a+b+c)
-    elif a == b and b == c and a == c:
-        answer = (a+b+c) * ((a*a) + (b*b) + (c*c)) * ((a*a*a)+(b*b*b)+(c*c*c))
+    check = len(set([a,b,c]))
+    if check == 1:
+        answer = (3*a) * (a**2*3) * (a**3*3)
+    elif check == 2:
+        answer = (a+b+c) * ((a**2)+(b**2)+(c**2))
     else:
-        answer = (a+b+c) * ((a*a) + (b*b) + (c*c))
+        answer = (a+b+c)
     return answer
