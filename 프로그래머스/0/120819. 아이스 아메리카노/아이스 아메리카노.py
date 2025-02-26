8a96d9ae40fb98de276ee562421362cb1f7df894
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-02-26 23:07:32 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,8 +1,3 @@
 def solution(money):
-    answer = []
-    tmp1=tmp2=money
-    coffee = tmp1 // 5500
-    re_money = tmp2 - (coffee*5500)
-    answer.append(coffee)
-    answer.append(re_money)
+    answer = [money // 5500, money % 5500]
     return answer
