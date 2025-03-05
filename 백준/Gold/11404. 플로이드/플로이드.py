58f5be71d28a2956cf55f2059f408681c6bce07d
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-03-05 21:22:49 +0900

    [Gold IV] Title: 플로이드, Time: 396 ms, Memory: 33564 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,33 +1,28 @@
-# 11401
 import sys
-INF = int(1e9)
-n = int(sys.stdin.readline()) # 도시 (노드)
-m = int(sys.stdin.readline()) # 버스 (간선)
-graph = [[INF]*(n+1) for _ in range(n+1)] # 최단경로 저장
+input = sys.stdin.readline
 
-# 자기 자신 지나는 경로 0으로 초기화
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a==b:
-            graph[a][b] = 0
+n = int(input()) # 도시개수
+m = int(input()) # 노선개수
+distance = [[sys.maxsize for j in range(n+1)] for i in range(n+1)]
+for i in range(n+1):
+    distance[i][i] = 0
+for i in range(m):
+    a,b,c = map(int, input().split())
+    # 최단경로만
+    if distance[a][b] > c:
+        distance[a][b] = c
 
-for _ in range(m):
-    a, b, c = map(int,sys.stdin.readline().split())
-    # 시작 도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있다.
-    # graph[a][b] = c
-    graph[a][b] = min(graph[a][b],c)
+#플로이드워셜 알고리즘
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if distance[i][j] > distance[i][k] + distance[k][j]:
+                distance[i][j] = distance[i][k] + distance[k][j]
 
-# 플로이드 워셜 수행
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
-
-# 최단경로 출력
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        if graph[a][b] == INF: # 갈 수 없다면 0으로 출력
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if distance[i][j] == sys.maxsize: # 갈수없으면 0
             print(0, end=" ")
         else:
-            print(graph[a][b], end=" ")
+            print(distance[i][j], end = " ")
     print()
