50330be92d929e54dadb04b3aa87bc23e0a638d0
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-03-14 17:42:43 +0900

    [Silver I] Title: 안전 영역, Time: 724 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,34 +1,53 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
+sys.setrecursionlimit(10**5)
+
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
-max_h = 0
-result = 0
+graph = []
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+max_h = 0 # 지역 최대 높이
 for i in range(n):
     tmp = max(graph[i])
-    max_h = max(tmp, max_h)
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
-def bfs(a, b, visited, h):
+    max_h = max(max_h,tmp)
+
+dx = [1,-1,0,0]
+dy = [0,0,-1,1]
+
+def dfs(x,y,h,visited):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+            if graph[nx][ny] > h:
+                visited[nx][ny] = True
+                dfs(nx,ny,h,visited)
+
+def bfs(x,y,h,visited):
     q = deque()
-    q.append((a,b))
-    visited[a][b] = 1
+    q.append((x,y))
     while q:
-        x, y = q.popleft()
+        a,b = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n and graph[nx][ny] > h and not visited[nx][ny]:
-                q.append((nx,ny))
-                visited[nx][ny] = 1
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+                if graph[nx][ny] > h:
+                    visited[nx][ny] = True
+                    q.append((nx,ny))
+result = 0
+# 지역 최대 높이만큼 탐색
 for h in range(max_h):
-    visited = [[0]*n for _ in range(n)]
+    visited = [[False]*n for _ in range(n)]
     cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j] and graph[i][j] > h:
-                bfs(i, j, visited, h)
+    for a in range(n):
+        for b in range(n):
+            if graph[a][b] > h and not visited[a][b]:
+                visited[a][b] = True
                 cnt += 1
-    if result < cnt : 
-        result = cnt
+                #dfs(a,b,h,visited)
+                bfs(a,b,h,visited)
+    result = max(cnt,result)
 print(result)
