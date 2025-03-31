bfdbcfb15e976384dae381e241f6246c8eee5e06
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-03-31 16:20:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1080 ms, Memory: 106832 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,21 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-a = [[0] * (n+1)] # index가 1부터인 원본 배열
-d = [[0] * (n+1) for _ in range(n+1)] # 합 배열
 
-# 원본 배열 입력
+# 원본배열
+d = []
 for i in range(n):
-    a_row = [0] + [int(x) for x in input().split()]
-    a.append(a_row)
+    d.append(list(map(int, input().split())))
 
-# 2차원 합 배열 생성
-for x in range(1,n+1):
-    for y in range(1,n+1):
-        d[x][y] = d[x-1][y] + d[x][y-1] - d[x-1][y-1] + a[x][y]
+# 누적합
+d_sum = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        d_sum[i][j] = d_sum[i-1][j] + d_sum[i][j-1] - d_sum[i-1][j-1] + d[i-1][j-1]
 
-# 정답 판 완성
+# 정답 출력
 for _ in range(m):
     x1,y1,x2,y2 = map(int, input().split())
-    sum = d[x2][y2] - d[x1-1][y2] - d[x2][y1-1] + d[x1-1][y1-1]
-    print(sum)
+    result = d_sum[x2][y2] - d_sum[x1-1][y2] - d_sum[x2][y1-1] + d_sum[x1-1][y1-1]
+    print(result)
